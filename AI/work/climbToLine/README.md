e4d770ed742ed5a98340ff8168fc97ecd3e666fd
stereomp3 <ericjjkkhandsome@gmail.com>
2023-02-23 11:48:41 +0800

    add README and modify climbToLine

---
+++ AI/work/climbToLine/README.md
@@ -0,0 +1,47 @@
+[code](climbToLine.py)
+
+最後結果: 
+
+* `p = [1.9000000000000015, 1.0600000000000007]`
+* `loss = 0.04399992752080731 `
+* `y_predicted= [1.9000000000000015, 2.960000000000002, 4.020000000000003, 5.080000000000004, 6.140000000000004]`
+* `y_origin = [1.9, 3.1, 3.9, 5.0, 6.2]`
+
+> climbToLine.py: 使用h = 0.01做爬山，使用老師原本設定的MSE做loss function，下面是主要添加的爬山程式碼
+
+```python
+def ArrayAdd(l1, l2, sub=False):  # 列表相加函數
+    l1c = l1.copy()
+    for i in range(len(l1c)):
+        if not sub:
+            l1c[i] += l2[i]
+        else:
+            l1c[i] -= l2[i]
+    return l1c
+
+
+def hillClimbing(f, hx, h=0.01):
+    h_x = hx.copy()  # 深拷貝一個新的p
+    while True:
+        In = False  # 檢測是否到山頂
+        for i in range(len(h_x)):  
+            dh = []
+            # 根據輸入的列表產生新的dh，這邊產生四個: [1,0]、[-1,0]、[0,1]、[0,-1]
+            for num in range(len(h_x)):  
+                if num == i:
+                    dh.append(h)
+                else:
+                    dh.append(0)
+            nh_x = ArrayAdd(h_x, dh)
+            nh_x2 = ArrayAdd(h_x, dh, True)
+            if f(nh_x) < f(h_x):
+                h_x = nh_x
+                In = True
+            if f(nh_x2) < f(h_x):
+                h_x = nh_x2
+                In = True
+        if not In:
+            break
+    return h_x
+```
+
--- AI/work/climbToLine/climbToLine.py
+++ AI/work/climbToLine/climbToLine.py
@@ -1,11 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from math import sqrt
 
-from regex import P
-
-# x = np.array([0, 1, 2, 3, 4], dtype=np.float32)
-# y = np.array([2, 3, 4, 5, 6], dtype=np.float32)
 x = np.array([0, 1, 2, 3, 4], dtype=np.float32)
 y = np.array([1.9, 3.1, 3.9, 5.0, 6.2], dtype=np.float32)
 
@@ -35,41 +30,31 @@ def ArrayAdd(l1, l2, sub=False):
     return l1c
 
 
-print(loss([2, 1]))
-
-
-# p = [0.0, 0.0]
-# plearn = optimize(loss, p, max_loops=3000, dump_period=1)
 def hillClimbing(f, hx, h=0.01):
     h_x = hx.copy()
-    h1 = [h, 0]
-    h2 = [0, h]
-    print(f(h_x + h1))
     while True:
-        print(h_x)
-        print(f(h_x))
         In = False
-        if f(ArrayAdd(h_x, h1)) < f(h_x):
-            h_x = ArrayAdd(h_x, h1)
-            In = True
-        if f(ArrayAdd(h_x, h1, True)) < f(h_x):
-            h_x = ArrayAdd(h_x, h1)
-            In = True
-        if f(ArrayAdd(h_x, h2)) < f(h_x):
-            h_x = ArrayAdd(h_x, h2)
-            In = True
-        if f(ArrayAdd(h_x, h2, True)) < f(h_x):
-            h_x = ArrayAdd(h_x, h2, True)
-            In = True
+        for i in range(len(h_x)):
+            dh = []
+            for num in range(len(h_x)):
+                if num == i:
+                    dh.append(h)
+                else:
+                    dh.append(0)
+            nh_x = ArrayAdd(h_x, dh)
+            nh_x2 = ArrayAdd(h_x, dh, True)
+            if f(nh_x) < f(h_x):
+                h_x = nh_x
+                In = True
+            if f(nh_x2) < f(h_x):
+                h_x = nh_x2
+                In = True
         if not In:
             break
     return h_x
 
 
 def optimize():
-    # 請修改這個函數，自動找出讓 loss 最小的 p
-    # p = [2,1] # 這個值目前是手動填的，請改為自動尋找。(即使改了 x,y 仍然能找到最適合的回歸線)
-    # p = [3,2] # 這個值目前是手動填的，請改為自動尋找。(即使改了 x,y 仍然能找到最適合的回歸線)
     p = [0.0, 0.0]
     return hillClimbing(loss, p)
 
